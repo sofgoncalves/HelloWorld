84e1032973806cbd2e3096e9e74c4ce7e53def65
Sofia Goncalves <21234118@shrewsbury.ac.uk>
2022-01-24 09:50:48 +0000

    lol

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,7 +6,12 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello World!");
+            Console.WriteLine("What's your name?");
+            string name = Console.ReadLine();
+            //Console.WriteLine(name);
+
+            Console.WriteLine($"Hello {name}.");
         }
     }
 }
